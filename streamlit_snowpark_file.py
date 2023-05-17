5b68f9afb2e885eb705b93f28078cc32feccffe8
Gerard Cuenca Ginesta <76474179+GerardCuenca1690@users.noreply.github.com>
2023-05-17 16:24:38 +0200

    Update streamlit_snowpark_file.py

--- streamlit_snowpark_file.py
+++ streamlit_snowpark_file.py
@@ -1,12 +1,33 @@
+import os
+import snowflake.connector
 import streamlit as st
 
-# Initialize connection.
-conn = st.experimental_connection('snowpark')
+# Retrieve Snowflake connection details from environment variables
+account = os.getenv("SNOWFLAKE_ACCOUNT")
+user = os.getenv("SNOWFLAKE_USER")
+password = os.getenv("SNOWFLAKE_PASSWORD")
+database = os.getenv("SNOWFLAKE_DATABASE")
+warehouse = os.getenv("SNOWFLAKE_WAREHOUSE")
 
-# Perform query.
+# Establish connection
+conn = snowflake.connector.connect(
+    user=user,
+    password=password,
+    account=account,
+    warehouse=warehouse,
+    database=database
+)
+
+# Create a cursor to execute queries
+cursor = conn.cursor()
+
+# Perform query
 query = 'SELECT * FROM "UEFA_PROD_DATAIKU"."MKT"."RFM_ANALYSIS_202303_OPTINS" WHERE "MACRO_THRESHOLD_MS" = \'Active\' LIMIT 5'
-df = conn.query(query, ttl=300)
+cursor.execute(query)
 
-# Print results.
-for row in df.itertuples():
-    st.write(f"{row.ID} has a :{row.OPT_IN_MS}:")
+# Fetch and print results
+for row in cursor:
+    st.write(f"{row[0]} has a :{row[1]}:")
+
+# Close the cursor and connection
+cursor.close()
