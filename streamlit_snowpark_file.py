9305c91310097304edbe9875df27a645611f3e1a
Gerard Cuenca Ginesta <76474179+GerardCuenca1690@users.noreply.github.com>
2023-05-24 13:39:33 +0200

    Update streamlit_snowpark_file.py

--- streamlit_snowpark_file.py
+++ streamlit_snowpark_file.py
@@ -2,32 +2,15 @@ import os
 import snowflake.connector
 import streamlit as st
 
-# Retrieve Snowflake connection details from environment variables
-account = os.getenv("SNOWFLAKE_ACCOUNT")
-user = os.getenv("SNOWFLAKE_USER")
-password = os.getenv("SNOWFLAKE_PASSWORD")
-database = os.getenv("SNOWFLAKE_DATABASE")
-warehouse = os.getenv("SNOWFLAKE_WAREHOUSE")
+conn = st.experimental_connection('snowpark')
 
-# Establish connection
-conn = snowflake.connector.connect(
-    user=user,
-    password=password,
-    account=account,
-    warehouse=warehouse,
-    database=database
-)
 
-# Create a cursor to execute queries
-cursor = conn.cursor()
-
+df = conn.query('SELECT * FROM "UEFA_PROD_DATAIKU"."MKT"."RFM_ANALYSIS_202303_OPTINS" LIMIT 5', ttl=600)
 # Perform query
-query = 'SELECT * FROM "UEFA_PROD_DATAIKU"."MKT"."RFM_ANALYSIS_202303_OPTINS" WHERE "MACRO_THRESHOLD_MS" = \'Active\' LIMIT 5'
+
 cursor.execute(query)
 
-# Fetch and print results
-for row in cursor:
+    
+for row in df.itertuples():
     st.write(f"{row[0]} has a :{row[1]}:")
 
-# Close the cursor and connection
-cursor.close()
